19955cfcc5ffb6c6f1619a1633abcc52c3fff29b
Manmeet567 <manmeet567singh@gmail.com>
2023-04-22 21:55:23 +0530

    wrapping up for today i.e 22-04-2023

--- src/pages/Settings/Settings.jsx
+++ src/pages/Settings/Settings.jsx
@@ -1,9 +1,24 @@
 import './Settings.css'
+import { RxCross2 } from 'react-icons/rx'
+import { useNavigate } from 'react-router-dom';
 
 function Settings() {
+
+    const navigate = useNavigate();
+
+    const handleClose = () => {
+        navigate('/channels/@me')
+    }
+
   return (
     <div className="settings-page">
-        
+        <div className="sp-title">
+            <h3>MY ACCOUNT</h3>
+            <RxCross2 onClick={handleClose} title='Close' style={{fontSize:'40px', borderRadius:'50%', padding:'5px' ,border:'2px solid #fff', cursor:'pointer'}} />
+        </div>
+        <div className="sp-options">
+            
+        </div>
     </div>
   )
 }
