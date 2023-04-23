e2747716902872c8fca2d532d78d1446854e2729
Manmeet567 <manmeet567singh@gmail.com>
2023-04-23 16:37:12 +0530

    added escape key functionality to settings page

--- src/pages/Settings/Settings.jsx
+++ src/pages/Settings/Settings.jsx
@@ -1,6 +1,7 @@
 import './Settings.css'
 import { RxCross2 } from 'react-icons/rx'
 import { useNavigate } from 'react-router-dom';
+import { useEffect } from 'react';
 
 function Settings() {
 
@@ -10,15 +11,33 @@ function Settings() {
         navigate('/channels/@me')
     }
 
+    const handleKeyDown = (event) => {
+    if (event.key === 'Escape') {
+      handleClose();
+    }
+  };
+
+  useEffect(() => {
+    document.addEventListener('keydown', handleKeyDown);
+
+    return () => {
+      document.removeEventListener('keydown', handleKeyDown);
+    };
+  }, []);
+
   return (
     <div className="settings-page">
-        <div className="sp-title">
-            <h3>MY ACCOUNT</h3>
-            <RxCross2 onClick={handleClose} title='Close' style={{fontSize:'40px', borderRadius:'50%', padding:'5px' ,border:'2px solid #fff', cursor:'pointer'}} />
+      <div className="sp-title">
+        <h3>MY ACCOUNT</h3>
+        <div className="esc-btn" onClick={handleClose} title='Close' >
+          <RxCross2 title='Close' style={{fontSize:'40px', borderRadius:'50%', padding:'5px' ,border:'2px solid #aeb3ba', cursor:'pointer'}} />
+          <p style={{fontSize:'12px'}}>ESC</p>
         </div>
-        <div className="sp-options">
-            
-        </div>
+      </div>
+
+      <div className="sp-profile-settings">
+        
+      </div>
     </div>
   )
 }
