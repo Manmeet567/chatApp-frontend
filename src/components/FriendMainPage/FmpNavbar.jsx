1f40ce445afbd175dd9127a11a075874999fcc62
Manmeet567 <manmeet567singh@gmail.com>
2023-05-07 12:33:16 +0530

    added a number notification icon in fmpNavbar's pending value

--- src/components/FriendMainPage/FmpNavbar.jsx
+++ src/components/FriendMainPage/FmpNavbar.jsx
@@ -2,6 +2,7 @@ import './FriendMainPage.css'
 import {FaUserFriends} from 'react-icons/fa'
 import {MdQuickreply} from 'react-icons/md'
 import {BsFillQuestionCircleFill} from 'react-icons/bs'
+import { useUserContext } from '../../hooks/useUserContext'
 
 function FmpNavbar({activeItem,setActiveItem}) {
 
@@ -9,13 +10,16 @@ function FmpNavbar({activeItem,setActiveItem}) {
     setActiveItem(str)
   }
 
+  const {pending} = useUserContext();
+  const pendingLength = pending?.length
+
   return (
     <div className="fmp-navbar" style={{userSelect:'none'}}>
         <ul className="fmp-options">
             <li><FaUserFriends className="f-icon" /><span>Friends</span></li>
             <li onClick={() => handleNavClick('online')} className={activeItem==='online' ? 'active':''}>Online</li>
             <li onClick={() => handleNavClick('all')} className={activeItem==='all' ? 'active':''}>All</li>
-            <li onClick={() => handleNavClick('pending')} className={activeItem==='pending' ? 'active':''}>Pending</li>
+            <li onClick={() => handleNavClick('pending')} className={activeItem==='pending' ? 'active':''} style={{display:'flex', alignItems:'center'}}>Pending {pendingLength && <div style={{marginLeft:'6px', width:'18px',height:'18px',display:'flex',alignItems:'center', justifyContent:'center', fontSize:'12px',fontWeight:'550', borderRadius:'50%', backgroundColor:'#f23f42', color:'#fff'}}className='fmpo-plength'>{pendingLength > 9 ? '9+' : pendingLength}</div>}</li>
             <li onClick={() => handleNavClick('blocked')} className={activeItem==='blocked' ? 'active':''}>Blocked</li>
             <li onClick={() => handleNavClick('addFriend')}><button>Add Friend</button></li>
         </ul>
