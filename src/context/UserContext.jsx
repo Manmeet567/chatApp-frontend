a118eee6697cef3ebe66b189001e7eb1de2f18c9
Manmeet567 <manmeet567singh@gmail.com>
2023-05-09 17:57:51 +0530

    added notification section in userContext

--- src/context/UserContext.jsx
+++ src/context/UserContext.jsx
@@ -39,7 +39,6 @@ export const userReducer = (state,action) => {
                 ...state,
                 notifications:action.payload
             }
-            
         
         default: return state;
     }
