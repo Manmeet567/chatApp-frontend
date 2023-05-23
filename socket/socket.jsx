965d704f4f6ea5637dd604181e5ecfa73e32bc02
Manmeet567 <manmeet567singh@gmail.com>
2023-05-23 11:42:09 +0530

    removed console logging token

--- socket/socket.jsx
+++ socket/socket.jsx
@@ -2,7 +2,7 @@ import io from 'socket.io-client';
 
 const data = JSON.parse(localStorage.getItem('user'));
 const token = data.token;
-console.log(token)
+
 const socket = io('ws://localhost:8900',{
     auth:{
         token:token
